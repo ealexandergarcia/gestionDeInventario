87f7d42781c9c55f14f74a4ae2ba88c0f07a88fd
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-03-21 19:10:56 -0500

    feat: :construction: Construccion de la db para proyexto final pro player HD 4K

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+
+# URL del servidor json-server
+url = 'http://localhost:3000/activos/1'
+
+# Enviar solicitud GET al servidor
+response = requests.get(url)
+
+# Verificar si la solicitud fue exitosa (código de estado 200)
+if response.status_code == 200:
+    # Obtener los datos en formato JSON
+    data = response.json()
+
+    # Verificar si existe la clave 'historialActivos' en los datos
+    if 'historialActivos' in data:
+        # Obtener los datos del historialActivos
+        historial_activos = data['historialActivos']
+
+        # Imprimir los datos del historialActivos
+        print(historial_activos)
+    else:
+        print("No se encontraron datos en historialActivos")
+else:
+    # Si la solicitud no fue exitosa, imprimir el código de estado
+    print("Error:", response.status_code)
